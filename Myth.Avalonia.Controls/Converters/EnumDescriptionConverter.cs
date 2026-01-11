8d1cdf9ee45e1d2be041a59c448a8182b704c6d1
Anton Polozhyi <apoloz@softserveinc.com>
2026-01-11 23:25:42 +0200

    added EnumDescriptionConverter

---
+++ Myth.Avalonia.Controls/Converters/EnumDescriptionConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+using Avalonia.Data.Converters;
+
+using Myth.Avalonia.Controls.Extensions;
+
+namespace Myth.Avalonia.Controls.Converters
+{
+	public class EnumDescriptionConverter : IValueConverter
+	{
+		public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+			=> value is Enum e ? e.GetDescription() : value?.ToString();
+
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+			=> throw new NotImplementedException();
+	}
+}
